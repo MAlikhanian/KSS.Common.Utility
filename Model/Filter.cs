cab6eaa8dfa2edd175de2c0983f9f43c29d7f77a
SPIAlikhanian01 <alikhanian.mohammad@gmail.com>
2025-03-01 21:08:10 +0330

    Add project files.

---
+++ Model/Filter.cs
@@ -0,0 +1,10 @@
+﻿using KSS.Common.Utility.Enum.Base;
+
+namespace KSS.Common.Utility.Model
+{
+    public class Filter
+    {
+        public DataType DataType { get; set; }
+        public object Value { get; set; }
+    }
+}
